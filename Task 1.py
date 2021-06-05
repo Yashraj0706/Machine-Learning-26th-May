a0e25311c19dce8edb9a1d46d6c0c717a90b1859
Yashraj0706 <84844645+Yashraj0706@users.noreply.github.com>
2021-06-05 15:47:20 +0530

    Add files via upload

---
+++ Task 1.py
@@ -0,0 +1,11 @@
+#word = input('Enter characters:')
+#char=input('enter the letter to find:')
+#count = 0
+#for i in word:
+ #   if i == char :
+  #      count = count + 1
+#print (count)
+import tkinter as tk
+app = tk.Tk()
+app.title('registration form')
+app.geometry('500x300')
